ca6e940729ddbdcd2142d8bc21b7baa72c194cf8
Trevor Saint <trevor@infigomedia.uk>
2016-09-13 10:07:01 +0100

    Global navigation style added. Javascript toggle functionality for mobile view added. Right column code sampel tweak

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -2,9 +2,8 @@
 
  'strict'
 
- // collapsibles
- if ($('.collapsibles').length > 0) {
-
+   // collapsibles
+  if ($('.collapsibles').length > 0) {
 
     $('.collapsibles-item').each(function() {
 
@@ -45,30 +44,23 @@
 
     });
 
-
   }
 
- // filters
- if ($('.filters').length > 0) {
-
+  // filters
+  if ($('.filters').length > 0) {
 
     $('.filters').each(function() {
 
-
       var $container = $(this);
-
 
       // click handler
       $container.find('.filters-button').on('click', function(e) {
 
-
         var $button     = $(this);
         var $dataToSort = $(this).attr('data-sort');
 
-
         // reset
         $container.find('tbody tr').attr('aria-hidden', null);
-
 
         // filter results
         if ($button.attr('data-sort') != 'all') {
@@ -82,17 +74,14 @@
 
       });
 
-
       // keydown handler
       $container.find('.filters-button').on('keydown', function(e) {
-
 
         // define current, previous and next (possible) tabs
         var $newButton;
         var $prevButton = $(this).prev();
         var $nextButton = $(this).next();
         var $dataToSort = $(this).attr('data-sort');
-
 
         // find the direction (prev or next)
         switch (e.keyCode) {
@@ -107,160 +96,153 @@
             break;
         }
 
-
         if ($newButton.length) {
           $newButton.focus();
         }
 
-
       });
 
-
     });
-
 
   }
 
- // tabs
- if ($('.tabs').length > 0) {
+  // tabs
+  if ($('.tabs').length > 0) {
 
+    $('.tabs').each(function() {
 
-  $('.tabs').each(function() {
+      var $container = $(this);
 
+      // the setup
+      $container.find('.tabs-header > ul').attr('role', 'tablist');
+      $container.find('.tabs-header > ul li').attr('role', 'presentation');
 
-    var $container = $(this);
-
-
-    // the setup
-    $container.find('.tabs-header > ul').attr('role', 'tablist');
-    $container.find('.tabs-header > ul li').attr('role', 'presentation');
-
-    $container.find('[role="tablist"] a').attr({
-      'role' : 'tab',
-      'tabindex' : '-1'
-    });
-
-
-    // make each aria-controls correspond id of targeted section (re href)
-    $container.find('[role="tablist"] a').each(function() {
-
-      $(this).attr(
-        'aria-controls', $(this).attr('href').substring(1)
-      );
-
-    });
-
-
-    // make first tab selected by default and allow it focus
-    $container.find('[role="tablist"] li:first-child a').attr({
-      'aria-selected' : true,
-      'tabindex' : 0
-    });
-
-
-    // make each section focusable and give it the tabpanel role
-    $container.find('section').attr({
-      'role' : 'tabpanel'
-    });
-
-
-    // make first child of each panel focusable programmatically
-    $container.find('section > *:first-child').attr({
-      'tabindex' : 0
-    });
-
-
-    // make all but the first section hidden (ARIA state and display CSS)
-    $container.find('[role="tabpanel"]:not(:first-of-type)').attr({
-      'aria-hidden' : true
-    });
-
-
-    // change focus between tabs with arrow keys
-    $container.find('[role="tab"]').on('keydown', function(e) {
-
-
-      // define current, previous and next (possible) tabs
-      var $original = $(this);
-      var $prev = $(this).parents('li').prev().children('[role="tab"]');
-      var $next = $(this).parents('li').next().children('[role="tab"]');
-      var $target;
-
-
-      // find the direction (prev or next)
-      switch (e.keyCode) {
-        case 37:
-          $target = $prev;
-          break;
-        case 39:
-          $target = $next;
-          break;
-        default:
-          $target = false;
-          break;
-      }
-
-
-      if ($target.length) {
-
-        $original.attr({
-          'tabindex' : -1,
-          'aria-selected' : null
-        });
-
-        $target.attr({
-          'tabindex' : 0,
-          'aria-selected' : true
-        }).focus();
-
-      }
-
-
-      // hide panels
-      $container.find('[role="tabpanel"]').attr('aria-hidden', true);
-
-
-      // show panel which corresponds to target
-      $('#' + $(document.activeElement).attr('href').substring(1)).attr('aria-hidden', null);
-
-
-    });
-
-
-    $container.find('[role="tab"]').on('click', function(e) {
-
-
-      e.preventDefault();
-
-
-      // remove focusability and aria-selected
-      $container.find('[role="tab"]').attr({
-        'tabindex' : -1,
-        'aria-selected' : null
+      $container.find('[role="tablist"] a').attr({
+        'role' : 'tab',
+        'tabindex' : '-1'
       });
 
+      // make each aria-controls correspond id of targeted section (re href)
+      $container.find('[role="tablist"] a').each(function() {
 
-      // replace above on clicked tab
-      $(this).attr({
+        $(this).attr(
+          'aria-controls', $(this).attr('href').substring(1)
+        );
+
+      });
+
+      // make first tab selected by default and allow it focus
+      $container.find('[role="tablist"] li:first-child a').attr({
         'aria-selected' : true,
         'tabindex' : 0
       });
 
+      // make each section focusable and give it the tabpanel role
+      $container.find('section').attr({
+        'role' : 'tabpanel'
+      });
 
-      // hide panels
-      $container.find('[role="tabpanel"]').attr('aria-hidden', true);
 
+      // make first child of each panel focusable programmatically
+      $container.find('section > *:first-child').attr({
+        'tabindex' : 0
+      });
 
-      // show corresponding panel
-      $('#' + $(this).attr('href').substring(1)).attr('aria-hidden', null);
+      // make all but the first section hidden (ARIA state and display CSS)
+      $container.find('[role="tabpanel"]:not(:first-of-type)').attr({
+        'aria-hidden' : true
+      });
 
+      // change focus between tabs with arrow keys
+      $container.find('[role="tab"]').on('keydown', function(e) {
+
+        // define current, previous and next (possible) tabs
+        var $original = $(this);
+        var $prev = $(this).parents('li').prev().children('[role="tab"]');
+        var $next = $(this).parents('li').next().children('[role="tab"]');
+        var $target;
+
+        // find the direction (prev or next)
+        switch (e.keyCode) {
+          case 37:
+            $target = $prev;
+            break;
+          case 39:
+            $target = $next;
+            break;
+          default:
+            $target = false;
+            break;
+        }
+
+        if ($target.length) {
+
+          $original.attr({
+            'tabindex' : -1,
+            'aria-selected' : null
+          });
+
+          $target.attr({
+            'tabindex' : 0,
+            'aria-selected' : true
+          }).focus();
+
+        }
+
+        // hide panels
+        $container.find('[role="tabpanel"]').attr('aria-hidden', true);
+
+        // show panel which corresponds to target
+        $('#' + $(document.activeElement).attr('href').substring(1)).attr('aria-hidden', null);
+
+      });
+
+      $container.find('[role="tab"]').on('click', function(e) {
+
+        e.preventDefault();
+
+        // remove focusability and aria-selected
+        $container.find('[role="tab"]').attr({
+          'tabindex' : -1,
+          'aria-selected' : null
+        });
+
+        // replace above on clicked tab
+        $(this).attr({
+          'aria-selected' : true,
+          'tabindex' : 0
+        });
+
+        // hide panels
+        $container.find('[role="tabpanel"]').attr('aria-hidden', true);
+
+        // show corresponding panel
+        $('#' + $(this).attr('href').substring(1)).attr('aria-hidden', null);
+
+      });
 
     });
 
+  }
 
-  });
+  // global navigation
+  if ($('.global-nav').length > 0) {
 
+    var $toggle = $('.global-nav-toggle');
+    var $menu   = $('.global-nav-menu');
 
- }
+    $toggle.on('click', function(e) {
+
+      e.preventDefault();
+
+      var state = $(this).attr('aria-expanded') === 'false' ? true : false;
+
+      $toggle.attr('aria-expanded', state);
+      $menu.attr('aria-expanded', state);
+
+    });
+
+  }
+
 
 })();
